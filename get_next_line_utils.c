94f37948958bc3a291a29c68e1fc7b236da6266e
Hydroponey <hydroponey@localhost>
2020-05-29 13:26:50 +0200

    Alloc an empty string when we didn't read any byte in the fd

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/24 15:47:57 by asimoes           #+#    #+#             */
-/*   Updated: 2020/05/29 12:45:08 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/05/29 13:25:40 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,13 @@ void		*ft_realloc(void *ptr, size_t size, size_t ptrsize)
 
 int			last_line(char **buf, char **line, int *bs, char *readbuf)
 {
+	if (*buf == NULL)
+	{
+		*line = (char*)malloc(1);
+		*line[0] = '\0';
+		free_buffers(readbuf, buf);
+		return (0);
+	}
 	*bs += 1;
 	if (process_buffer(buf, line, bs, '\0') == -1)
 	{
