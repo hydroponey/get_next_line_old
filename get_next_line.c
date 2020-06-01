5a5f8e9480c9b14752f5ac36d27974519c9e63d5
Hydroponey <hydroponey@localhost>
2020-06-01 21:44:06 +0200

    clean version

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,43 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/01 16:00:32 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/01 18:19:58 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/01 21:13:00 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-t_fd_data		*get_fd_data(t_fd_data **start, int fd)
+int				free_fd_data(t_fd_data **start)
 {
 	t_fd_data	*cur;
 
+	while (*start)
+	{
+		cur = *start;
+		*start = cur->next;
+		free(cur->buf);
+		free(cur);
+	}
+	free(*start);
+	return (-1);
+}
+
+t_fd_data		*get_fd_data(t_fd_data **start, int fd)
+{
+	t_fd_data	*new;
+	t_fd_data	*cur;
+	t_fd_data	*last;
+
 	cur = *start;
-	if (cur == NULL)
-		return (NULL);
 	while (cur)
 	{
 		if (cur->fd == fd)
 			return (cur);
+		if (cur->next == NULL)
+			last = cur;
 		cur = cur->next;
 	}
-	return (NULL);
-}
-
-t_fd_data		*add_fd_data(t_fd_data **start, int fd)
-{
-	t_fd_data	*new;
-	t_fd_data	*copy;
-
 	if (!(new = malloc(sizeof(t_fd_data))))
 		return (NULL);
 	new->buf = NULL;
@@ -41,16 +51,9 @@ t_fd_data		*add_fd_data(t_fd_data **start, int fd)
 	new->next = NULL;
 	new->size = 0;
 	if (*start == NULL)
-	{
 		*start = new;
-	}
 	else
-	{
-		copy = *start;
-		while (copy->next != NULL)
-			copy = copy->next;
-		copy->next = new;
-	}
+		last->next = new;
 	return (new);
 }
 
@@ -60,15 +63,16 @@ int				get_line(t_fd_data *d, char **line)
 {
 	char			*newbuf;
 	char			*endl;
+	int				i;
 
 	if (d->buf == NULL)
-		return (-2);
+		return (ERR_BUFFER_NULL);
 	if ((endl = ft_strnchr(d->buf, '\n', d->size)))
 	{
 		if (!(*line = ft_strndup(d->buf, (endl - d->buf))))
-			return (-1);
+			return (ERR_MALLOC);
 		if (!(newbuf = malloc(d->size - (endl - d->buf) - 1)))
-			return (-1);
+			return (ERR_MALLOC);
 		d->size -= (endl - d->buf) + 1;
 		ft_memcpy(newbuf, d->buf + (endl - d->buf) + 1, d->size);
 		free(d->buf);
@@ -78,10 +82,10 @@ int				get_line(t_fd_data *d, char **line)
 	else if (d->eof)
 	{
 		if (!(*line = ft_strndup(d->buf, d->size)))
-			return (-1);
+			return (ERR_MALLOC);
 		return (remove_fd_data(d));
 	}
-	return (-2);
+	return (ERR_NO_LINE);
 }
 
 int				read_loop(t_fd_data *d, char **line)
@@ -91,19 +95,26 @@ int				read_loop(t_fd_data *d, char **line)
 	int		gl_value;
 
 	if (!(readbuf = malloc(BUFFER_SIZE)))
-		return (-1);
+		return (ERR_MALLOC);
 	while ((readlen = read(d->fd, readbuf, BUFFER_SIZE)) >= 0)
 	{
-		d->eof = (readlen == 0) ? 1 : 0;
-		gl_value = get_line(d, line);
-		if (gl_value != -2)
-			return (gl_value);
 		if (!(d->buf = ft_realloc(d->buf, d->size + readlen, d->size)))
-			return (-1);
+		{
+			free(readbuf);
+			return (ERR_MALLOC);
+		}
 		ft_memcpy(d->buf + d->size, readbuf, readlen);
 		d->size += readlen;
+		d->eof = (readlen == 0) ? 1 : 0;
+		gl_value = get_line(d, line);
+		if (gl_value != ERR_NO_LINE && gl_value != ERR_BUFFER_NULL)
+		{
+			free(readbuf);
+			return (gl_value);
+		}
 	}
-	return (-1);
+	free(readbuf);
+	return (ERR_READ_FAILURE);
 }
 
 int				get_next_line(int fd, char **line)
@@ -114,15 +125,15 @@ int				get_next_line(int fd, char **line)
 	int					retval;
 
 	if (!(cur = get_fd_data(&fd_data, fd)))
-	{
-		if (!(cur = add_fd_data(&fd_data, fd)))
-			return (-1);
-	}
+		return (free_fd_data(&fd_data));
 	gl_value = get_line(cur, line);
-	if (gl_value != -2)
+	if (gl_value == ERR_MALLOC)
+		return (free_fd_data(&fd_data));
+	else if (gl_value != ERR_NO_LINE && gl_value != ERR_BUFFER_NULL)
 		return (gl_value);
-	if ((retval = read_loop(cur, line)) == -1)
-		return (-1);
+	retval = read_loop(cur, line);
+	if (retval == ERR_READ_FAILURE || retval == ERR_MALLOC)
+		return (free_fd_data(&fd_data));
 	return (retval);
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/23 02:19:51 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/01 17:52:58 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/01 20:55:10 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+
+# define	ERR_BUFFER_NULL		-1
+# define	ERR_NO_LINE			-2
+# define 	ERR_MALLOC			-3
+# define	ERR_READ_FAILURE	-4
 
 typedef struct	s_fd_data
 {
@@ -25,15 +30,18 @@ typedef struct	s_fd_data
 	void			*next;
 }				t_fd_data;
 
-char	*ft_strndup(const char *str, unsigned int n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-char	*ft_strnchr(const char *s, int c, int len);
-void	*ft_realloc(void *ptr, size_t size, size_t ptrsize);
-int		remove_fd_data(t_fd_data *fd_data);
+char			*ft_strndup(const char *str, unsigned int n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+char			*ft_strnchr(const char *s, int c, int len);
+void			*ft_realloc(void *ptr, size_t size, size_t ptrsize);
 
-int		process_buffer(char **buffer, char **line, int *bufsize, int c);
-int		free_buffers(char *readbuf, char **buf);
-int		get_next_line(int fd, char **line);
-int		get_next_line_r(t_fd_data **fd_data, int fd, char **line);
+t_fd_data		*get_fd_data(t_fd_data **start, int fd);
+t_fd_data		*add_fd_data(t_fd_data **start, int fd);
+int				remove_fd_data(t_fd_data *fd_data);
+
+int				get_line(t_fd_data *d, char **line);
+int				read_loop(t_fd_data *d, char **line);
+
+int				get_next_line(int fd, char **line);
 
 #endif
